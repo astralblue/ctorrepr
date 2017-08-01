b31fe5b87bfaac68bf7d27799ee61320537d82e1
Eugene M. Kim <eugene.kim@ntti3.com>
2017-07-31 20:02:21 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='ctorrepr',
-    version='0.1.0',
+    version='0.1.1',
     description="A mix-in that provides __repr__() from constructor "
                 "arguments.",
     long_description=readme + '\n\n' + history,
